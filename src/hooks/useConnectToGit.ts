44fbc23fbbbfc5697dda28f65f52d18e14ff8b05
LATEAM99 <egonzl92@outlook.com>
2025-02-20 20:08:56 -0800

    implements api fetching and required constants

---
+++ src/hooks/useConnectToGit.ts
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { PullRequest } from "../models/pullrequest.model";
+import { API_BASE, OWNER, PULLS, REPO } from "../utils/constants";
+
+
+export function useConnectToGit() {
+    const [pullRequests, setPullRequest] = useState<PullRequest[]>([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState<string | null>(null);
+
+    useEffect(()=>{
+        const fetchPullRequest = async (owner: string, repository: string) => {
+            try {
+                setLoading(true);
+                const {data} = await axios.get(`${API_BASE}/${owner}/${repository}/${PULLS}`);
+                setPullRequest(data)
+
+            } catch(err: unknown) {
+                if (axios.isAxiosError(err)) {
+                    setError(err.response?.data?.message || "Failed to fetch PRs.");
+                  } else {
+                    setError("An unexpected error occurred.");
+                  }
+            } finally {
+                setLoading(false);
+            }
+        }
+        fetchPullRequest(OWNER, REPO);
+    },[]);
+
+    return {pullRequests, loading, error}
+}
---
+++ src/utils/constants.ts
@@ -0,0 +1,5 @@
+export const API_BASE = `https://api.github.com/repos/`
+export const PULLS = `pulls`;
+
+export const OWNER = "divvydose";
+export const REPO = "ui-coding-challenge";
